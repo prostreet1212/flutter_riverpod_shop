b3bcac305058cd979b80fca5af1fbac4b14ccbfd
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2022-11-13 13:47:29 +0300

    riverpod

---
+++ lib/data/list_menu.dart
@@ -0,0 +1,17 @@
+import 'model/menu.dart';
+
+class ListMenu{
+
+
+  List<CoffeMenu> listMenu=<CoffeMenu>[
+    CoffeMenu('latte.jpg','Латте' , 80),
+    CoffeMenu('latte_grusha.jpg','Латте груша' , 100),
+    CoffeMenu('malina_kokos.jpg','Раф малина-кокос' , 100),
+    CoffeMenu('raf.jpg','Раф' , 100),
+  ];
+
+  Future<List<CoffeMenu>> getListMenu()async{
+    return listMenu;
+  }
+
+}
---
+++ lib/data/model/menu.dart
@@ -0,0 +1,8 @@
+class CoffeMenu{
+  String image;
+  String name;
+  double price;
+  bool isBuy=false;
+
+  CoffeMenu(this.image, this.name, this.price);
+}
