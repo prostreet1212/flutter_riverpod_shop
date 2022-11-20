f8b9ae8598e6567092692197d045d9d3ef2b474c
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2022-11-20 22:51:07 +0300

    riverpod version

--- lib/ui/coffee_shop.dart
+++ lib/ui/coffee_shop.dart
@@ -57,7 +57,7 @@ class CoffeeShop extends ConsumerWidget {
                               ),
                               onPressed: () {
                                 menuNotifier.buyNot(menu);
-                                badgeNotifier.changBadgeCount(menu);
+                                badgeNotifier.changBadgeCount(menu);1
                               },
                             ),
 
